0cc455ee1552c71cf14f16c11d89fbdd82cdad27
Daniel Seiler <seda1019@h-ka.de>
2021-08-26 17:57:42 +0200

    added task 2.1 to 2.7

---
+++ joinsAndViews.sql
@@ -0,0 +1,23 @@
+-- 2.1
+SELECT auftrnr, datum FROM auftrag WHERE kundnr IN (SELECT nr FROM kunde WHERE name = 'Fahrrad Shop');
+
+-- 2.2
+SELECT auftrnr, datum FROM auftrag WHERE kundnr =SOME (SELECT nr FROM kunde WHERE name = 'Fahrrad Shop');
+
+-- 2.3
+SELECT auftrnr, datum FROM auftrag WHERE EXISTS (SELECT nr FROM kunde WHERE kunde.nr=auftrag.kundnr AND name = 'Fahrrad Shop');
+
+-- 2.4
+SELECT kundnr, COUNT(kundnr) AS anzahl, MIN(datum) AS von, MAX(datum) AS bis FROM auftrag GROUP BY kundnr ORDER BY kundnr ASC;
+
+-- 2.5
+SELECT kundnr, COUNT(kundnr) AS anzahl, MIN(datum) AS von, MAX(datum) AS bis FROM auftrag GROUP BY kundnr HAVING COUNT(kundnr) <= 1 ORDER BY kundnr ASC;
+
+-- 2.6
+SELECT kunde.nr, kunde.name, auftrag.auftrnr FROM kunde INNER JOIN auftrag ON kunde.nr = auftrag.kundnr ORDER BY kunde.nr;
+
+-- 2.7
+SELECT kunde.nr, kunde.name, auftrag.auftrnr, personal.name FROM kunde INNER JOIN auftrag ON kunde.nr = auftrag.kundnr INNER JOIN personal ON personal.persnr = auftrag.persnr ORDER BY kunde.nr;
+
+-- 2.8
+
