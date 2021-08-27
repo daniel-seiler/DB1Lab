08e79e262706c7ab0598a252410409f26061c3a5
Daniel Seiler <seda1019@h-ka.de>
2021-08-27 23:01:09 +0200

    added task 2.8 to 2.11

--- joinsAndViews.sql
+++ joinsAndViews.sql
@@ -20,4 +20,14 @@ SELECT kunde.nr, kunde.name, auftrag.auftrnr FROM kunde INNER JOIN auftrag ON ku
 SELECT kunde.nr, kunde.name, auftrag.auftrnr, personal.name FROM kunde INNER JOIN auftrag ON kunde.nr = auftrag.kundnr INNER JOIN personal ON personal.persnr = auftrag.persnr ORDER BY kunde.nr;
 
 -- 2.8
+SELECT kunde.name, COUNT(auftrag.kundnr) AS anzahl FROM kunde INNER JOIN auftrag ON kunde.nr = auftrag.kundnr GROUP BY kunde.name ORDER BY anzahl DESC LIMIT 1;
+
+-- 2.9
+WITH bestekunden(name, anzahl) AS (SELECT kunde.name, COUNT(auftrag.kundnr) AS anzahl FROM kunde INNER JOIN auftrag ON kunde.nr = auftrag.kundnr GROUP BY kunde.name ORDER BY anzahl DESC) SELECT * FROM bestekunden LIMIT 1;
+
+-- 2.10
+CREATE VIEW KundenUmsatz AS SELECT kunde.name, SUM(auftragsposten.gesamtpreis) FROM auftragsposten INNER JOIN auftrag ON auftrag.auftrnr=auftragsposten.auftrnr INNER JOIN kunde ON kunde.nr = auftrag.kundnr GROUP BY kunde.name;
+
+-- 2.11
+DROP VIEW KundenUmsatz;
 
