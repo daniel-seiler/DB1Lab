cfdc6960930c6103eadb6b1c9dc3ac8191d6fe2c
Paul Lorenz <mail@paul-lorenz.de>
2021-11-17 19:12:50 +0100

    Fix spaghetti code, add task 4.3

--- JDBC/src/main/java/de/hska/iwii/db1/jdbc/JDBCQueries.java
+++ JDBC/src/main/java/de/hska/iwii/db1/jdbc/JDBCQueries.java
@@ -1,31 +1,16 @@
 package de.hska.iwii.db1.jdbc;
 
 import java.sql.*;
-/*
-    TASK 4.2
- */
-public class JDBCQueryResult {
-    private ResultSet rs;
-    private final Statement statement;
-    private final String query;
 
-    public JDBCQueryResult(Connection connection, String query) throws SQLException{
-        this.statement = connection.createStatement(
-                ResultSet.TYPE_SCROLL_INSENSITIVE,
-                ResultSet.CONCUR_UPDATABLE);
-        this.query = query;
+public class JDBCQueries {
+
+    private final Connection connection;
+
+    public JDBCQueries(Connection connection) throws SQLException{
+        this.connection = connection;
     }
 
-    private void execute() {
-        try {
-            this.rs = statement.executeQuery(query);
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-    }
-
-    private void printHeader() throws SQLException {
-        ResultSetMetaData rsmd = rs.getMetaData();
+    private void printHeader(ResultSetMetaData rsmd) throws SQLException {
         int numberOfColumns = rsmd.getColumnCount();
         StringBuilder sb = new StringBuilder();
         String cell;
@@ -65,14 +50,14 @@ public class JDBCQueryResult {
         System.out.println(sb.toString());
     }
 
-    private void printBody() throws SQLException {
-        ResultSetMetaData rdms = rs.getMetaData();
-        int numberOfColumns = rdms.getColumnCount();
+    private void printBody(ResultSet rs) throws SQLException {
+        ResultSetMetaData rsmd = rs.getMetaData();
+        int numberOfColumns = rsmd.getColumnCount();
         rs.first();
         while (rs.next()) {
             for (int i = 1; i <= numberOfColumns; i++) {
-                int columnWidth = rdms.getColumnDisplaySize(i);
-                boolean isInt = rdms.getColumnTypeName(i).startsWith("int");
+                int columnWidth = rsmd.getColumnDisplaySize(i);
+                boolean isInt = rsmd.getColumnTypeName(i).startsWith("int");
                 // replace '|' with space when end of line is reached
                 char e = (i == numberOfColumns) ? ' ' : '|';
                 String template = isInt ? "%" + columnWidth + "s " + e : " %-" + columnWidth + "s" + e;
@@ -82,9 +67,46 @@ public class JDBCQueryResult {
         }
     }
 
-    public void printResult() throws SQLException {
-        printHeader();
-        printBody();
+    /*
+    Task 4.2
+    print staff with staff no, place, occupation
+     */
+    public void printStaff() throws SQLException {
+        String query = "SELECT persnr, name, ort, aufgabe FROM personal";
+        Statement statement = connection.createStatement(
+                ResultSet.TYPE_SCROLL_INSENSITIVE,
+                ResultSet.CONCUR_UPDATABLE);
+        ResultSet rs = statement.executeQuery(query);
+        ResultSetMetaData rsmd = rs.getMetaData();
+        printHeader(rsmd);
+        printBody(rs);
+    }
+
+    /*
+    Task 4.3
+    print client <-> supplier relationship with optional client search
+     */
+    public void printClientsWithSuppliers(String search) throws SQLException {
+        String query = "SELECT k.name as kunde, k.nr as knr, lant.name as lieferant, lant.nr as lnr\n" +
+                "FROM auftragsposten as ap\n" +
+                "INNER JOIN lieferung lung on ap.teilnr = lung.teilnr\n" +
+                "INNER JOIN lieferant lant on lung.liefnr = lant.nr\n" +
+                "INNER JOIN auftrag a on a.auftrnr = ap.auftrnr\n" +
+                "INNER JOIN kunde k on k.nr = a.kundnr\n" +
+                "WHERE lower(k.name) LIKE lower(?)";
+        PreparedStatement statement = connection.prepareStatement(
+                query,
+                ResultSet.TYPE_SCROLL_INSENSITIVE,
+                ResultSet.CONCUR_UPDATABLE);
+        statement.setString(1, search);
+        ResultSet rs = statement.executeQuery();
+        ResultSetMetaData rsmd = rs.getMetaData();
+        printHeader(rsmd);
+        printBody(rs);
+    }
+
+    public void printClientsWithSuppliers() throws SQLException {
+        printClientsWithSuppliers("%");
     }
 
     public static void main (String[] args) {
@@ -94,14 +116,10 @@ public class JDBCQueryResult {
 
         JDBCFoo pgsql = new JDBCFoo(url, username, password);
         Connection conn = pgsql.getConnection();
-
-        String query = "SELECT persnr, name, ort, aufgabe FROM personal";
         try {
-            JDBCQueryResult result = new JDBCQueryResult(conn, query);
-            result.execute();
-            result.printResult();
-            result.execute();
-            result.printResult();
+            JDBCQueries shop_db = new JDBCQueries(conn);
+            shop_db.printStaff();
+            shop_db.printClientsWithSuppliers("%Fahr%");
         } catch (SQLException e) {
             e.getMessage();
         }
