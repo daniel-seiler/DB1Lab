55de16093c358d4213628bfa611a018a4400d63d
Daniel Seiler <73987329+daniel-seiler@users.noreply.github.com>
2022-01-13 17:43:07 +0100

    added creation of table entries

--- DBLab-ORM-Projekt/src/main/java/de/hska/iwii/db1/jpa/JPAApplication.java
+++ DBLab-ORM-Projekt/src/main/java/de/hska/iwii/db1/jpa/JPAApplication.java
@@ -1,28 +1,91 @@
 package de.hska.iwii.db1.jpa;
 
+import java.sql.Date;
+import java.sql.Time;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+import javax.persistence.Query;
 
 public class JPAApplication {
-	private EntityManagerFactory entityManagerFactory;
-
-	public JPAApplication() {
-		Logger.getLogger("org.hibernate").setLevel(Level.ALL);
-		entityManagerFactory = Persistence.createEntityManagerFactory("DB1");
-	}
-	
-	public void testFlights() {
-	}
-	
-	public EntityManagerFactory getEntityManagerFactory() {
-		return entityManagerFactory;
-	}
-	
-	public static void main(String[] args) {
-		JPAApplication app = new JPAApplication();
-		app.testFlights();
-	}
+    private EntityManagerFactory entityManagerFactory;
+    private EntityManager em;
+    
+    public JPAApplication() {
+        Logger.getLogger("org.hibernate").setLevel(Level.ALL);
+        entityManagerFactory = Persistence.createEntityManagerFactory("DB1");
+        this.em = this.entityManagerFactory.createEntityManager();
+    }
+    
+    public void testFlights() {
+        Customer c1 = addCustomer(0, "Margarete", "Eisenhardt", "mega.eisenhardt@web.de");
+        Customer c2 = addCustomer(1, "Dorothea", "Krasnewicz", "doro.krasnewicz@yahoo.com");
+        Flight f1 = addFlight(0, "NER236", new Time(12039493), "Guangzhou Baiyun International Airport");
+        Flight f2 = addFlight(1, "RHN563", new Time(190587), "Bad Kissing Airfield");
+        Flight f3 = addFlight(2, "ABC109", new Time(192385), "Fak Fak Airport");
+        Booking b1 = addBooking(0, f1, c1, 2, new Date(298346));
+        Booking b2 = addBooking(1, f2, c1, 2, new Date(985341));
+        Booking b3 = addBooking(2, f2, c2, 2, new Date(725834));
+        Booking b4 = addBooking(3, f3, c2, 2, new Date(569348));
+        
+        String hql = "SELECT k.forename FROM Customer k";
+        Query q = em.createQuery(hql);
+        List<Object> resultList = q.getResultList();
+        
+        for (Object temp : resultList) {
+            System.out.println(temp);
+        }
+        em.close();
+    }
+    
+    public Flight addFlight(long id, String flightNumber, Time startTime, String startAirport) {
+        Flight flight = new Flight()
+                .setNum(flightNumber)
+                .setStartTime(startTime)
+                .setStartAirport(startAirport)
+                .setId(id);
+        em.getTransaction().begin();
+        em.persist(flight);
+        em.getTransaction().commit();
+        return flight;
+    }
+    
+    public Customer addCustomer(long id, String forename, String surname, String email) {
+        Customer customer = new Customer()
+                .setForename(forename)
+                .setSurname(surname)
+                .setEmail(email)
+                .setId(id);
+        em.getTransaction().begin();
+        em.persist(customer);
+        em.getTransaction().commit();
+        return customer;
+    }
+    
+    public Booking addBooking(long id, Flight flight, Customer customer, int numBookedSeats, Date date) {
+        Booking booking = new Booking()
+                .setFlight(flight)
+                .setCustomer(customer)
+                .setNumBookedSeats(numBookedSeats)
+                .setDate(date)
+                .setId(id);
+        em.getTransaction().begin();
+        em.persist(customer);
+        em.getTransaction().commit();
+        return booking;
+    }
+    
+    public EntityManagerFactory getEntityManagerFactory() {
+        return entityManagerFactory;
+    }
+    
+    public static void main(String[] args) {
+        JPAApplication app = new JPAApplication();
+        app.testFlights();
+        app.getEntityManagerFactory().close();
+    }
 }
